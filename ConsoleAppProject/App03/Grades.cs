ec5ea8f8b35ce3216cc15f2b4fe85b7a5b58a5c5
Daniils Pucko <40274564+DaniilsPucko@users.noreply.github.com>
2021-02-04 14:12:00 +0000

    Initial commit

---
+++ ConsoleAppProject/App03/Grades.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleAppProject.App03
+{
+    /// <summary>
+    /// Grade A is First Class   : 70 - 100
+    /// Grade B is Upper Second  : 60 - 69
+    /// Grade C is Lower Second  : 50 - 59
+    /// Grade D is Third Class   : 40 - 49
+    /// Grade F is Fail          :  0 - 39
+    /// </summary>
+    public enum Grades
+    {
+        [Description("Fail")]
+        F, 
+        [Description("Third Class")]
+        D, 
+        [Description("Lower Second")]
+        C, 
+        [Description("Upper Second")]
+        B, 
+        [Description("First Class")]
+        A
+    }
+}
---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAppProject
+{
+    /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.  It will be used
+    /// to start Apps 01 to 05 for CO453 CW1
+    /// 
+    /// This Project has been modified by:
+    /// Derek Peacock 14/12/2020
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+            Console.WriteLine();
+            Console.Beep();
+
+
+        }
+    }
+}
