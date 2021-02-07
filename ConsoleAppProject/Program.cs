14d2a428e886d81b4f4adcfd0b5d61ac6347d752
Daniils Pucko <mondex123@gmail.com>
2021-02-07 14:59:54 +0000

    added my name

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleAppProject
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Daniils Pucko 07.02.2021
     /// </summary>
     public static class Program
     {
