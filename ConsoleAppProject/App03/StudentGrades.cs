c424080931ba5a63d9ea4dd2d4711365e516cd24
Daniils Pucko <mondex123@gmail.com>
2021-03-14 19:24:45 +0000

    added empty methods

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -37,5 +37,41 @@ namespace ConsoleAppProject.App03
             Marks = new int[Students.Length];
 
         }
+
+        /// <summary>
+        /// Input a mark between 0-100 for each student
+        /// and store it in Marks array.
+        /// </summary>
+        public void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// List all the students and display their name
+        /// and current mark.
+        /// </summary>
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convert a student mark to a grade
+        /// from F - fail to A - first class.
+        /// </summary>
+        public Grades ConvertToGrade(int mark)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate and display minimum,
+        /// maximum and mean mark for all the students.
+        /// </summary>
+        public void CalculateStats()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
