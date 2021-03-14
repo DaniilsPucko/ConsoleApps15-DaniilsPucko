e0f02ba6fe0796e45202831c7087c4cd5a615346
Daniils Pucko <mondex123@gmail.com>
2021-03-14 19:17:51 +0000

    added variables and constructor

---
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App03
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StudentGrades
+    {
+        public string[] Students { get; set; }
+
+        public int[] Marks { get; set; }
+
+        public int[] GradeProfile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+        public StudentGrades()
+        {
+            Students = new string[]
+            {
+                "Daniel", "German", "Hanzo", "Max",
+                "Hatori", "Kayla", "Victor", "Lane",
+                "Natalie", "Arthur"
+            };
+
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+
+        }
+    }
+}
