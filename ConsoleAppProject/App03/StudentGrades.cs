4108d54df23b867fa6ae8aa783d87711c2ccab69
Daniils Pucko <mondex123@gmail.com>
2021-03-14 19:43:52 +0000

    added constant grades

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -21,6 +21,14 @@ namespace ConsoleAppProject.App03
 
         public int Maximum { get; set; }
 
+        public const int LowestMark = 0;
+        public const int HighestMark = 100;
+
+        public const int LowestGradeD = 40;
+        public const int LowestGradeC = 50;
+        public const int LowestGradeB = 60;
+        public const int LowestGradeA = 70;
+
         /// <summary>
         /// constructor 
         /// </summary>
@@ -62,7 +70,11 @@ namespace ConsoleAppProject.App03
         /// </summary>
         public Grades ConvertToGrade(int mark)
         {
-            throw new NotImplementedException();
+            if (mark >= 0 && mark < 40)
+            {
+                return Grades.F;
+            }
+            else return Grades.D;
         }
 
         /// <summary>
