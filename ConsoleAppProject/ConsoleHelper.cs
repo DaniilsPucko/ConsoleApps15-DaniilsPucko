a2c9c8be494dc9422b16b03b987d7c09c2252093
Daniils Pucko <mondex123@gmail.com>
2021-03-14 17:26:23 +0000

    modified consolehelper

--- ConsoleAppProject/ConsoleHelper.cs
+++ ConsoleAppProject/ConsoleHelper.cs
@@ -40,10 +40,6 @@ namespace ConsoleAppProject
             Console.WriteLine($"               {title}              ");
             Console.WriteLine("          by Daniils Pucko          ");
             Console.WriteLine("----------------------------------\n");
-            Console.WriteLine("Select distance unit to convert from:");
-            Console.WriteLine("1: Miles");
-            Console.WriteLine("2: Feet");
-            Console.WriteLine("3: Metres");
         }
     }
 }
