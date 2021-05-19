becd0d76dd2621e576e3f4cd2ba5508837ddfab0
Daniils Pucko <mondex123@gmail.com>
2021-05-19 12:47:11 +0100

    did refactoring

--- ConsoleAppProject/App04/MessagePost.cs
+++ ConsoleAppProject/App04/MessagePost.cs
@@ -35,37 +35,6 @@ namespace ConsoleAppProject.App04
             Message = text;
         }
 
-        /// <summary>
-        /// Record one more 'Like' indication from a user.
-        /// </summary>
-        public void Like()
-        {
-            likes++;
-        }
-
-        ///<summary>
-        /// Record that a user has withdrawn his/her 'Like' vote.
-        ///</summary>
-        public void Unlike()
-        {
-            if (likes > 0)
-            {
-                likes--;
-            }
-        }
-
-        ///<summary>
-        /// Add a comment to this post.
-        /// </summary>
-        /// <param name="text">
-        /// The new comment to add.
-        /// </param>        
-        public void AddComment(String text)
-        {
-            comments.Add(text);
-        }
-
-
         ///<summary>
         /// Display the details of this post.
         /// 
@@ -99,33 +68,5 @@ namespace ConsoleAppProject.App04
             }
         }
 
-        ///<summary>
-        /// Create a string describing a time point in the past in terms 
-        /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
-        /// Currently, only seconds and minutes are used for the string.
-        /// </summary>
-        /// <param name="time">
-        ///  The time value to convert (in system milliseconds)
-        /// </param> 
-        /// <returns>
-        /// A relative time string for the given time
-        /// </returns>      
-        private String FormatElapsedTime(DateTime time)
-        {
-            DateTime current = DateTime.Now;
-            TimeSpan timePast = current - time; 
-
-            long seconds = (long)timePast.TotalSeconds;
-            long minutes = seconds / 60;
-            
-            if (minutes > 0)
-            {
-                return minutes + " minutes ago";
-            }
-            else
-            {
-                return seconds + " seconds ago";
-            }
-        }
     }
 }
