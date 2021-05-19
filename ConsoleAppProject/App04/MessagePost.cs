8d6661fc9d9e7b7dc9635ca554fc7b16bda98301
Daniils Pucko <mondex123@gmail.com>
2021-05-19 12:34:44 +0100

    added modified by comment

--- ConsoleAppProject/App04/MessagePost.cs
+++ ConsoleAppProject/App04/MessagePost.cs
@@ -12,6 +12,10 @@ namespace ConsoleAppProject.App04
     /// Michael Kölling and David J. Barnes
     /// version 0.1
     /// </author>
+    /// <modified>
+    /// Daniils Pucko
+    /// 16.05.2021
+    /// </modified>
     public class MessagePost
     {
         private int likes;
